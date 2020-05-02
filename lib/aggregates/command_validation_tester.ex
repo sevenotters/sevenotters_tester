1475386f216e42a303073d6bcb890e52912eb998
“nicolafiorillo” <nicola.fiorillo@gmail.com>
2020-05-02 08:42:33 +0200

    Shutdown useless aggregate.

--- lib/aggregates/command_validation_tester.ex
+++ lib/aggregates/command_validation_tester.ex
@@ -3,34 +3,60 @@ defmodule SevenottersTester.CommandValidationTester do
 
   defstruct number: nil
 
-  @apply_syntact_validation_command "ApplySyntactValidation"
+  @apply_syntax_validation_command "ApplySyntaxValidation"
+  @apply_semantic_validation_command "ApplySemanticValidation"
 
   @moduledoc """
     Aggregate to test command validation.
     Responds to commands:
-    - #{@apply_syntact_validation_command}
+    - #{@apply_syntax_validation_command}
+    - #{@apply_semantic_validation_command}
   """
 
   defp init_state, do: %__MODULE__{}
 
   @spec route(String.t(), any) :: {:routed, Map.y(), atom} | {:invalid, Map.t()}
-  def route(@apply_syntact_validation_command, params) do
+  def route(@apply_syntax_validation_command, params) do
     cmd = %{
       number: params[:number],
       data: params[:data]
     }
 
-    @apply_syntact_validation_command
+    @apply_syntax_validation_command
+    |> Seven.Otters.Command.create(cmd)
+    |> validate_param()
+  end
+
+  def route(@apply_semantic_validation_command, params) do
+    cmd = %{
+      number: params[:number],
+      data: params[:data]
+    }
+
+    @apply_semantic_validation_command
     |> Seven.Otters.Command.create(cmd)
     |> validate_param()
   end
 
   def route(_command, _params), do: :not_routed
 
+  @spec pre_handle_command(Map.t(), any) :: {:managed, List.t()}
+  defp pre_handle_command(%Seven.Otters.Command{type: @apply_semantic_validation_command, payload: %{data: :invalid_semantic_data}} = _command, _state) do
+    {:routed_but_invalid, "invalid_semantic_data"}
+  end
+
+  defp pre_handle_command(%Seven.Otters.Command{type: @apply_semantic_validation_command, payload: %{data: :useless_aggregate_data}} = _command, _state) do
+    {:no_aggregate, "useless_aggregate_data"}
+  end
+
   defp pre_handle_command(_command, _state), do: :ok
 
   @spec handle_command(Map.t(), any) :: {:managed, List.t()}
-  defp handle_command(%Seven.Otters.Command{type: @apply_syntact_validation_command} = _command, _state) do
+  defp handle_command(%Seven.Otters.Command{type: @apply_syntax_validation_command} = _command, _state) do
+    {:managed, []}
+  end
+
+  defp handle_command(%Seven.Otters.Command{type: @apply_semantic_validation_command} = _command, _state) do
     {:managed, []}
   end
 
--- test/command_validation_test.exs
+++ test/command_validation_test.exs
@@ -1,35 +1,88 @@
 defmodule CommandValidationTest do
   use ExUnit.Case
 
-  test "send invalid data in command" do
-    number = TestHelper.new_number()
+  describe "Syntax validation" do
+    test "send invalid data in command" do
+      number = TestHelper.new_number()
 
-    result =
-      %Seven.CommandRequest{
-        id: TestHelper.new_id(),
-        command: "ApplySyntactValidation",
-        sender: __MODULE__,
-        params: %{number: number, data: :bad}
-      }
-      |> Seven.CommandBus.send_command_request()
+      result =
+        %Seven.CommandRequest{
+          id: TestHelper.new_id(),
+          command: "ApplySyntaxValidation",
+          sender: __MODULE__,
+          params: %{number: number, data: :bad}
+        }
+        |> Seven.CommandBus.send_command_request()
 
-    assert result == {:routed_but_invalid, "invalid_data"}
-    refute TestHelper.get_aggregate(SevenottersTester.CommandValidationTester, number)
+      assert result == {:routed_but_invalid, "invalid_data"}
+      refute TestHelper.get_aggregate(SevenottersTester.CommandValidationTester, number)
+    end
+
+    test "send valid data in command" do
+      number = TestHelper.new_number()
+
+      result =
+        %Seven.CommandRequest{
+          id: TestHelper.new_id(),
+          command: "ApplySyntaxValidation",
+          sender: __MODULE__,
+          params: %{number: number, data: :good}
+        }
+        |> Seven.CommandBus.send_command_request()
+
+      assert result == :managed
+      assert TestHelper.get_aggregate(SevenottersTester.CommandValidationTester, number)
+    end
   end
 
-  test "send valid data in command" do
-    number = TestHelper.new_number()
+  describe "Semantic validation" do
+    test "send invalid data in command" do
+      number = TestHelper.new_number()
 
-    result =
-      %Seven.CommandRequest{
-        id: TestHelper.new_id(),
-        command: "ApplySyntactValidation",
-        sender: __MODULE__,
-        params: %{number: number, data: :good}
-      }
-      |> Seven.CommandBus.send_command_request()
+      result =
+        %Seven.CommandRequest{
+          id: TestHelper.new_id(),
+          command: "ApplySemanticValidation",
+          sender: __MODULE__,
+          params: %{number: number, data: :invalid_semantic_data}
+        }
+        |> Seven.CommandBus.send_command_request()
 
-    assert result == :managed
-    assert TestHelper.get_aggregate(SevenottersTester.CommandValidationTester, number)
+      assert result == {:routed_but_invalid, "invalid_semantic_data"}
+      assert TestHelper.get_aggregate(SevenottersTester.CommandValidationTester, number)
+    end
+
+    test "send valid data in command" do
+      number = TestHelper.new_number()
+
+      result =
+        %Seven.CommandRequest{
+          id: TestHelper.new_id(),
+          command: "ApplySemanticValidation",
+          sender: __MODULE__,
+          params: %{number: number, data: :valid_semantic_data}
+        }
+        |> Seven.CommandBus.send_command_request()
+
+      assert result == :managed
+      assert TestHelper.get_aggregate(SevenottersTester.CommandValidationTester, number)
+    end
+
+    test "send invalid data in command for useless aggregate" do
+      number = TestHelper.new_number()
+
+      result =
+        %Seven.CommandRequest{
+          id: TestHelper.new_id(),
+          command: "ApplySemanticValidation",
+          sender: __MODULE__,
+          params: %{number: number, data: :useless_aggregate_data}
+        }
+        |> Seven.CommandBus.send_command_request()
+
+      assert result == {:no_aggregate, "useless_aggregate_data"}
+      Process.sleep(500)
+      refute TestHelper.get_aggregate(SevenottersTester.CommandValidationTester, number)
+    end
   end
 end
